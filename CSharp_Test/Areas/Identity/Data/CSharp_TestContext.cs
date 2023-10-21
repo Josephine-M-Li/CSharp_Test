058217561ec2b739cc79cc3a4c9d211e001cc5a4
rayli <rayrhan312@gmail.com>
2023-10-21 21:55:21 +0800

    Add User Class

---
+++ CSharp_Test/Areas/Identity/Data/CSharp_TestContext.cs
@@ -0,0 +1,22 @@
+﻿using CSharp_Test.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharp_Test.Data;
+
+public class CSharp_TestContext : IdentityDbContext<CSharp_TestUser>
+{
+    public CSharp_TestContext(DbContextOptions<CSharp_TestContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
