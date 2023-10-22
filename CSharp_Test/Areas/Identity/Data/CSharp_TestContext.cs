2c2ae2d7ab0f185bebc52d707bcac799298a49ac
rayli <rayrhan312@gmail.com>
2023-10-22 20:22:35 +0800

    用EF Core Power Tools建立model

--- CSharp_Test/Areas/Identity/Data/CSharp_TestContext.cs
+++ CSharp_Test/Areas/Identity/Data/CSharp_TestContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CSharp_Test.Data;
 
@@ -18,5 +20,31 @@ public class CSharp_TestContext : IdentityDbContext<CSharp_TestUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new CSharp_TestUserEntityConfiguration());
+
     }
+}
+
+public class CSharp_TestUserEntityConfiguration:IEntityTypeConfiguration<CSharp_TestUser>
+{
+
+    public void Configure(EntityTypeBuilder<CSharp_TestUser> builder)
+    {
+        builder.Property(u => u.name).HasMaxLength(255);
+        builder.Property(u => u.email).HasMaxLength(255);
+    }
+
+    //public Guid id { get; set; }
+    //public string org_id { get; set; }
+    //public string name { get; set; }
+    //public string birthday { get; set; }
+    //public string email { get; set; }
+    //public string account { get; set; }
+    //public string password { get; set; }
+    //public int status { get; set; }
+    //public DateTime created_at { get; set; }
+    //public DateTime updated_at { get; set; }
+
+
 }
--- CSharp_Test/Areas/Identity/Data/CSharp_TestUser.cs
+++ CSharp_Test/Areas/Identity/Data/CSharp_TestUser.cs
@@ -9,5 +9,17 @@ namespace CSharp_Test.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the CSharp_TestUser class
 public class CSharp_TestUser : IdentityUser
 {
+    public Guid id { get; set; }
+    public string org_id { get; set; }
+    public string name { get; set; }
+    public string birthday { get; set; }
+    public string email { get; set; }
+    public string account { get; set; }
+    public string password { get; set; }
+    public int status { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+
+
 }
 
